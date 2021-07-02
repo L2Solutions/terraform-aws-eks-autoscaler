4f27e9c74ab5cad1369c393fdccb924a0b56bc6b
Nathan Papapietro <npapapietro95@gmail.com>
2021-07-02 15:53:16 -0500

    Examples

---
+++ examples/autoscaler-deploy/main.tf
@@ -0,0 +1,26 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+module "autoscaler" {
+  source = "../.."
+
+  name                    = "cluster-autoscaler"
+  cluster_id              = "mycluster"
+  cluster_oidc_issuer_url = "<my oidc url>"
+  cluster_version         = "1.20"
+
+  autoscaler_nodeselector = {
+    "nodes" = "services"
+    "cpu"   = "xlarge"
+  }
+  autoscaler_tolerations = [{
+    "key"    = "zone"
+    "value"  = "true"
+    "effect" = "NO_SCHEDULE"
+  }]
+
+
+}
