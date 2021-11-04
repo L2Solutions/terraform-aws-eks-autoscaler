7f5d2e5513a6efe6b616a034ec82913a992e9ba8
Nathan Papapietro <npapapietro95@gmail.com>
2021-11-04 16:54:26 -0500

    Daemonset Bug

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "iam_role_arn" {
   description = "ARN of IAM eks autoscaler role"
   value       = aws_iam_role.this.arn
 }
+
+output "lc_test" {
+  description = "ARN of IAM eks autoscaler role"
+  value       = "-${data.aws_ssm_parameter.this_gpu.value}-"
+}
